1c3dd95083928a4ea1d238ff0a01e73b80e267cf
JRAllum <allum434@gmail.com>
2019-07-26 16:59:02 +0200

    Initial commit

---
+++ SimpleChess/Rook.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SimpleChess
+{
+     public class Rook : Piece
+    {
+       public string type = "Rook";
+
+
+        public Rook()
+        {
+            Symbol = "TRN";
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            if (type == "Rook") return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+            return true;
+        }
+    }
+}
